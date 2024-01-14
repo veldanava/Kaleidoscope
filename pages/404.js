f11095727b1d6865df30951623e27061bfea7961
Kiana Dimitri <maisan098704@gmail.com>
2024-01-14 20:59:53 +0700

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -11,12 +11,12 @@ import {
 const NotFound = () => {
   return (
     <Container>
-      <Heading as="h1">Not found</Heading>
+      <Heading as="h1">404 Error</Heading>
       <Text>The page you&apos;re looking for was not found.</Text>
       <Divider my={6} />
       <Box my={6} align="center">
         <NextLink href="/" passHref>
-          <Button colorScheme="teal">Return to home</Button>
+          <Button colorScheme="teal">Return to homepage</Button>
         </NextLink>
       </Box>
     </Container>
